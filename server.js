40f16e5da294ad3bcaa12e86ccdcbdcd0d119862
Tejaswi Gowda <tejaswi@asu.edu>
2024-01-11 14:52:11 -0700

    ac

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+// static page
+app.use(express.static(path.join(__dirname, './docs')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// start server
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!');
+});
